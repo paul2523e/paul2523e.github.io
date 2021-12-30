36d4645c37bcccea4b8c4a47e5eaea7b6ace00df
Paul Arriz <paul2523e@yahoo.com.ar>
2021-12-29 22:41:17 -0500

    updated map

--- static/script.js
+++ static/script.js
@@ -1,9 +1,10 @@
 var margin = {top: 2, right: 2, bottom: 2, left: 2}
 width = document.getElementById('GlassManufacturingCanvasDiv').offsetWidth - margin.left - margin.right
-console.log("width :",width)
-height = window.innerHeight - margin.top - margin.bottom 
 
-var projection = d3.geoMercator().translate([width/2, height/2]);
+height = document.getElementById('GlassManufacturingCanvasDiv').offsetWidth - margin.top - margin.bottom 
+
+
+var projection = d3.geoMercator().translate([width*3/5, height*2/5]);
 var path = d3.geoPath().projection(projection);
 
 var MyCountries = ["Peru","Ecuador","Argentina","Brazil","Mexico","Chile","Colombia","United States of America","Canada","Portugal","Italy"]
@@ -18,8 +19,7 @@ var svg = d3.select("#map")
 
 var tooltip = d3.select("body").append("div")
   .style("opacity", 0)
-  .attr("class", "tooltip")
-  .style("background-color", "gray")
+  .attr("class", "tooltip w3-white")
   .style("border", "solid")
   .style("border-width", "1px")
   .style("border-radius", "5px")
@@ -37,7 +37,7 @@ function createMap(worldfeatures) {
   
   
   svg.append("g").attr("id", "neighboorhoods")
-    .attr("transform", "scale(1.2)")
+    .attr("transform", "scale(1.5)")
     .selectAll("path")
     .data(worldfeatures)
     .enter()
@@ -45,30 +45,35 @@ function createMap(worldfeatures) {
     .attr("class","country")
     .attr("d", path)
     .attr("fill", function(d){ 
-        //console.log(gameDataFiltered);
-        
-        if(MyCountries.includes(d.properties.name)){
-            
-            return "blue"
-            
-        }else{
-            return "grey"
-        }
-        
-    })
+        return MyCountries.includes(d.properties.name) ? "teal" :  "grey"
+        }        
+    )
     .on("mouseover", function(event,d) {
       tooltip.transition()
         .duration(200)
-        .style("opacity", .5);
+        .style("opacity", .9);
       })
       .on("mousemove", function(event,d) {
 
-        tooltip.html("<p>"+d.properties.name+"<br></p>")
+        tooltip.html(CreateToolTipHtml(d))
           .style("left", (event.pageX) + "px")
           .style("top", (event.pageY - 2) + "px");
         })
-    .on('mouseout', tooltip.transition().duration(200).style("opacity", 0))
+    .on('mouseout', function(event,d)
+    {
+      console.log("mouse out")
+      tooltip.transition()
+      .duration(200)
+      .style("opacity", 0)
+    }
+    )
   ;
+
+  function CreateToolTipHtml(country_feature){
+    return "<p class=\"w3-text-grey\">"+country_feature.properties.name+"<br></p>"
+    // pending to read file with info about projects (i.e. scv) and maybe add interactivty/animations to 
+    //display graphically the information.
+  }
   
   // From https://www.d3indepth.com/zoom-and-pan/:
 }
